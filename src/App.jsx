4436ab947d6e0c837acda7281330077b96722bad
Raman Sharma <kukreti.rs@gmail.com>
2024-01-07 13:05:44 +0530

    1. Configured the store. 2. Added Header and Footer components. 3. Added tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-import conf from "./conf/conf";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>My Blog</h1>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return !isLoading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
